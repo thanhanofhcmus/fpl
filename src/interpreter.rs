45e889ba7efebdfa7a2854e687a97c93a58afdb8
annt22 <annt22@vng.com.vn>
2024-03-19 00:10:20 +0700

    move if

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -3,38 +3,7 @@ use crate::token::Token;
 
 pub fn interpret(ast: AstNode) -> Result<Value, String> {
     match ast {
-        AstNode::Binary { l, op, r } => {
-            let le = interpret(*l)?;
-            let re = interpret(*r)?;
-            match op {
-                Token::Plus | Token::Minus | Token::Star | Token::Slash => {
-                    let (Value::Number(a), Value::Number(b)) = (&le, &re) else {
-                        return Err(format!("expect number, have {:?}, {:?}", le, re));
-                    };
-                    let v = match op {
-                        Token::Plus => a + b,
-                        Token::Minus => a - b,
-                        Token::Star => a * b,
-                        Token::Slash => a / b,
-                        _ => panic!(),
-                    };
-                    Ok(Value::Number(v))
-                }
-                Token::And | Token::Or => {
-                    let (Value::Bool(a), Value::Bool(b)) = (&le, &re) else {
-                        return Err(format!("expect boolean, have {:?}, {:?}", le, re));
-                    };
-                    let (a, b) = (*a, *b);
-                    let v = match op {
-                        Token::And => a && b,
-                        Token::Or => a || b,
-                        _ => panic!(),
-                    };
-                    Ok(Value::Bool(v))
-                }
-                _ => Err("invalid op".to_owned()),
-            }
-        }
+        AstNode::Binary { l, op, r } => binary(*l, op, *r),
         AstNode::If {
             cond,
             true_,
@@ -49,5 +18,38 @@ pub fn interpret(ast: AstNode) -> Result<Value, String> {
             Ok(v)
         }
         AstNode::Primary(v) => Ok(v),
+    }
+}
+
+fn binary(l: AstNode, op: Token, r: AstNode) -> Result<Value, String> {
+    let le = interpret(l)?;
+    let re = interpret(r)?;
+    match op {
+        Token::Plus | Token::Minus | Token::Star | Token::Slash => {
+            let (Value::Number(a), Value::Number(b)) = (&le, &re) else {
+                return Err(format!("expect number, have {:?}, {:?}", le, re));
+            };
+            let v = match op {
+                Token::Plus => a + b,
+                Token::Minus => a - b,
+                Token::Star => a * b,
+                Token::Slash => a / b,
+                _ => panic!(),
+            };
+            Ok(Value::Number(v))
+        }
+        Token::And | Token::Or => {
+            let (Value::Bool(a), Value::Bool(b)) = (&le, &re) else {
+                return Err(format!("expect boolean, have {:?}, {:?}", le, re));
+            };
+            let (a, b) = (*a, *b);
+            let v = match op {
+                Token::And => a && b,
+                Token::Or => a || b,
+                _ => panic!(),
+            };
+            Ok(Value::Bool(v))
+        }
+        _ => Err("invalid op".to_owned()),
     }
 }
